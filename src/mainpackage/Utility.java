cc358e21ab6118467a578245132222af18d365f5
AqeelJamali777 <134383790+AqeelJamali777@users.noreply.github.com>
2023-05-23 18:38:04 +0500

    Update Utility.java

--- src/mainpackage/Utility.java
+++ src/mainpackage/Utility.java
@@ -1,3 +1,4 @@
+package mainpackage;
 public class UtilityMethods {
     public static double calculateCPN(float matriculationPercentage, float intermediatePercentage, int testResult) {
         double CPN = matriculationPercentage * 0.1 + intermediatePercentage * 0.3 + testResult * 0.6;
