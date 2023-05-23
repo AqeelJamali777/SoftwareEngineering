02614ea89e3ec2dcdbfc09542f79cedb4edc5680
Asadullah6 <115351450+Asadullah6@users.noreply.github.com>
2023-05-23 18:31:33 +0500

    Update and rename src/cpn.java to src/mainpackage/cpn.java

---
+++ src/mainpackage/cpn.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class MainClass {
+    public static void main(String[] args) {
+        Scanner marks1 = new Scanner(System.in);
+
+        System.out.println("Enter matriculation percentage accurate=");
+        float matriculationPercentage = marks1.nextFloat();
+
+        System.out.println("Enter intermediate percentage accurate=");
+        float intermediatePercentage = marks1.nextFloat();
+
+        System.out.println("Enter test score=");
+        int testResult = marks1.nextInt();
+
+        double CPN = calculateCPN(matriculationPercentage, intermediatePercentage, testResult);
+
+        System.out.println("Your CPN would be = " + CPN);
+    }
