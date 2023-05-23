101eaa4591d8ddb2f6963c4e4ee0d758423cd1bf
Asadullah6 <115351450+Asadullah6@users.noreply.github.com>
2023-05-23 18:30:15 +0500

    Create Utility.java

---
+++ src/Utility.java
@@ -0,0 +1,6 @@
+public class UtilityMethods {
+    public static double calculateCPN(float matriculationPercentage, float intermediatePercentage, int testResult) {
+        double CPN = matriculationPercentage * 0.1 + intermediatePercentage * 0.3 + testResult * 0.6;
+        return CPN;
+    }
+}
